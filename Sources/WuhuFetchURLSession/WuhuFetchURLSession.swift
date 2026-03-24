f77809b12529bf2d3ac8d9a364ab90ffc1760835
Minsheng Liu <lambda@liu.ms>
2026-03-24 12:28:49 +0800

    Implement URLSession fetch adapter (#1)
    * Implement URLSession fetch adapter
    
    * Use main branch Fetch dependency

--- Sources/WuhuFetchURLSession/WuhuFetchURLSession.swift
+++
@@ -1,9 +0,0 @@
-import WuhuFetch
-
-public struct WuhuFetchURLSession: Sendable {
-  public let upstream: WuhuFetch
-
-  public init(upstream: WuhuFetch = .init()) {
-    self.upstream = upstream
-  }
-}
